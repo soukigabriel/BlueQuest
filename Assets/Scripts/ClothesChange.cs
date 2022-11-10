6ba102a009872dcfe8cd90f7f33c32997ad36d67
Gabriel Souki <gabrielsouki@gmail.com>
2022-11-09 21:04:33 -0400

    Front and back change of clothe done

--- Assets/Scripts/ClothesChange.cs
+++ Assets/Scripts/ClothesChange.cs
@@ -5,19 +5,35 @@ using UnityEngine;
 public class ClothesChange : MonoBehaviour
 {
 
-
-    //Front character sprites
+    [Header("Front Clothes Sprites")]
+    public Sprite frontHatSpriteOne;
+    public Sprite frontHatSpriteTwo;
+    public Sprite frontBodyClotheSpriteOne;
+    public Sprite frontBodyClotheSpriteTwo;
     public Sprite frontLeftArmClotheSprite;
     public Sprite frontRightArmClotheSprite;
     public Sprite frontLeftLegClotheSprite;
     public Sprite frontRightLegClotheSprite;
 
+    [Space]
+    [Header("Front Naked Sprites")]
     public Sprite frontLeftArmNakedSprite;
     public Sprite frontRightArmNakedSprite;
     public Sprite frontLeftLegNakedSprite;
     public Sprite frontRightLegNakedSprite;
 
-    //UI Character
+    [Space]
+    [Header("Front character reference Sprites")]
+    public SpriteRenderer frontHat;
+    public SpriteRenderer frontBodyClothes;
+    public SpriteRenderer frontLeftArm;
+    public SpriteRenderer frontRightArm;
+    public SpriteRenderer frontWeaponSprite;
+    public SpriteRenderer frontLeftLeg;
+    public SpriteRenderer frontRightLeg;
+
+    [Space]
+    [Header("UI character Sprites")]
     public SpriteRenderer InventoryFrontHat;
     public SpriteRenderer InventoryFrontBodyClothes;
     public SpriteRenderer InventoryFrontLeftArm;
@@ -26,37 +42,65 @@ public class ClothesChange : MonoBehaviour
     public SpriteRenderer InventoryFrontLeftLeg;
     public SpriteRenderer InventoryFrontRightLeg;
 
-    //Front Character
-    public SpriteRenderer frontHat;
-    public SpriteRenderer frontBodyClothes;
-    public SpriteRenderer frontLeftArm;
-    public SpriteRenderer frontRightArm;
-    public SpriteRenderer weaponSprite;
-    public SpriteRenderer frontLeftLeg;
-    public SpriteRenderer frontRightLeg;
+
+    [Space]
+    [Header("Back character reference Sprites")]
+    public SpriteRenderer backHat;
+    public SpriteRenderer backBodyClothes;
+    public SpriteRenderer backLeftArm;
+    public SpriteRenderer backRightArm;
+    public SpriteRenderer backWeaponSprite;
+    public SpriteRenderer backLeftLeg;
+    public SpriteRenderer backRightLeg;
+
+    [Header("Back Clothes Sprites")]
+    public Sprite backHatSpriteOne;
+    public Sprite backHatSpriteTwo;
+    public Sprite backBodyClotheSpriteOne;
+    public Sprite backBodyClotheSpriteTwo;
+    public Sprite backLeftArmClotheSprite;
+    public Sprite backRightArmClotheSprite;
+    public Sprite backLeftLegClotheSprite;
+    public Sprite backRightLegClotheSprite;
+
+    [Space]
+    [Header("Back Naked Sprites")]
+    public Sprite backLeftArmNakedSprite;
+    public Sprite backRightArmNakedSprite;
+    public Sprite backLeftLegNakedSprite;
+    public Sprite backRightLegNakedSprite;
 
     //Hat
     public void ShowHat()
     {
         frontHat.enabled = true;
         InventoryFrontHat.enabled = true;
+        backHat.enabled = true;
     }
     public void HideHat()
     {
         frontHat.enabled = false;
         InventoryFrontHat.enabled = false;
+        backHat.enabled = false;
     }
-    public void ChangeHat(Sprite newSprite)
+    public void SetHatOne()
     {
-        frontHat.sprite = newSprite;
-        InventoryFrontHat.sprite = newSprite;
+        frontHat.sprite = frontHatSpriteOne;
+        InventoryFrontHat.sprite = frontHatSpriteOne;
+        backHat.sprite = backHatSpriteOne;
+    }
+    public void SetHatTwo()
+    {
+        frontHat.sprite = frontHatSpriteTwo;
+        InventoryFrontHat.sprite = frontHatSpriteTwo;
+        backHat.sprite = backHatSpriteTwo;
     }
 
 
     //Clothes
     public void ShowClothe()
     {
-        //Show character clothe
+        //Show front character clothe
         frontBodyClothes.enabled = true;
         frontLeftArm.sprite = frontLeftArmClotheSprite;
         frontRightArm.sprite = frontRightArmClotheSprite;
@@ -69,10 +113,17 @@ public class ClothesChange : MonoBehaviour
         InventoryFrontRightArm.sprite = frontRightArmClotheSprite;
         InventoryFrontLeftLeg.sprite = frontLeftLegClotheSprite;
         InventoryFrontRightLeg.sprite = frontRightLegClotheSprite;
+
+        //Show back character clothe
+        backBodyClothes.enabled = true;
+        backLeftArm.sprite = backLeftArmClotheSprite;
+        backRightArm.sprite = backRightArmClotheSprite;
+        backLeftLeg.sprite = backLeftLegClotheSprite;
+        backRightLeg.sprite = backRightLegClotheSprite;
     }
     public void HideClothe()
     {
-        //Hide character clothe
+        //Hide front character clothe
         frontBodyClothes.enabled = false;
         frontLeftArm.sprite = frontLeftArmNakedSprite;
         frontRightArm.sprite = frontRightArmNakedSprite;
@@ -85,30 +136,50 @@ public class ClothesChange : MonoBehaviour
         InventoryFrontRightArm.sprite = frontRightArmNakedSprite;
         InventoryFrontLeftLeg.sprite = frontLeftLegNakedSprite;
         InventoryFrontRightLeg.sprite = frontRightLegNakedSprite;
+
+        //Hide back character clothe
+        backBodyClothes.enabled = false;
+        backLeftArm.sprite = backLeftArmNakedSprite;
+        backRightArm.sprite = backRightArmNakedSprite;
+        backLeftLeg.sprite = backLeftLegNakedSprite;
+        backRightLeg.sprite = backRightLegNakedSprite;
+
     }
-    public void SetClothe(Sprite frontBodyClothe)
+    public void SetClotheOne()
     {
         ShowClothe();
-        frontBodyClothes.sprite = frontBodyClothe;
-        InventoryFrontBodyClothes.sprite = frontBodyClothe;
+        frontBodyClothes.sprite = frontBodyClotheSpriteOne;
+        InventoryFrontBodyClothes.sprite = frontBodyClotheSpriteOne;
+        backBodyClothes.sprite = backBodyClotheSpriteOne;
+    }
+
+    public void SetClotheTwo()
+    {
+        ShowClothe();
+        frontBodyClothes.sprite = frontBodyClotheSpriteTwo;
+        InventoryFrontBodyClothes.sprite = frontBodyClotheSpriteTwo;
+        backBodyClothes.sprite = backBodyClotheSpriteTwo;
     }
 
     public void ShowWeapon()
     {
-        weaponSprite.enabled = true;
+        frontWeaponSprite.enabled = true;
         InventoryWeaponSprite.enabled = true;
+        backWeaponSprite.enabled = true;
     }
 
     public void HideWeapon()
     {
-        weaponSprite.enabled = false;
+        frontWeaponSprite.enabled = false;
         InventoryWeaponSprite.enabled = false;
+        backWeaponSprite.enabled = false;
     }
 
     public void SetWeapon(Sprite newWeaponSprite)
     {
         ShowWeapon();
-        weaponSprite.sprite = newWeaponSprite;
+        frontWeaponSprite.sprite = newWeaponSprite;
         InventoryWeaponSprite.sprite = newWeaponSprite;
+        backWeaponSprite.sprite = newWeaponSprite;
     }
 }
